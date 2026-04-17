2e519ce478c9d9ba3b704489ae6f1fc97b164992
Ali Islam <wekadaana123@gmail.com>
2026-04-17 22:17:27 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("heloword")),
+        body: Center(child: Text("ali weka")),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    ),
+  );
+}
